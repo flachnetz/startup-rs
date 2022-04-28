400efc41aa67d41392fa4bd2e061b2bcbccafad1
Oliver Bestmann <oliver.bestmann@googlemail.com>
2022-04-28 11:42:00 +0200

    Configure zipkin in an extra package.

--- startup-base/src/lib.rs
+++ startup-base/src/lib.rs
@@ -2,14 +2,18 @@ use atty::Stream;
 use figment::providers::{Env, Format, Yaml};
 use figment::Error;
 use figment::Figment;
-use opentelemetry::sdk::trace;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
+use tracing_subscriber::reload::Handle;
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{Layer, Registry};
+use parking_lot::RwLock;
+use tracing_subscriber::util::SubscriberInitExt;
 
-mod idgenerator;
+lazy_static::lazy_static! {
+    static ref TRACING_LAYER: RwLock<Option<Handle<Option<Box<dyn Layer<Registry>+Send+Sync>>, Registry>>> = RwLock::new(None);
+}
 
 #[macro_export]
 macro_rules! init {
@@ -43,11 +47,6 @@ fn extract_with_default<C: Default + Serialize + DeserializeOwned>(default_yaml:
 struct BaseConfig {
     #[serde(default)]
     verbose: bool,
-
-    // TODO probably move into a monitoring package
-    zipkin: String,
-    // TODO add metrics
-    // statsd: HostPort,
 }
 
 pub fn init<C: Default + Serialize + DeserializeOwned>(service_name: &str, config: &str) -> Result<C, Error> {
@@ -63,30 +62,21 @@ pub fn init<C: Default + Serialize + DeserializeOwned>(service_name: &str, confi
         LevelFilter::INFO
     };
 
-    opentelemetry::global::set_text_map_propagator(opentelemetry_zipkin::Propagator::new());
+    // build a dynamic handler that can be set later
+    let (dynamic_layer, reload_handle) = tracing_subscriber::reload::Layer::new(None);
 
-    let trace_config = trace::Config::default()
-        .with_id_generator(idgenerator::IdGenerator64);
+    // set the handle so we can set the filter later on.
+    *TRACING_LAYER.write() = Some(reload_handle);
 
-    let tracer = opentelemetry_zipkin::new_pipeline()
-        .with_service_name(service_name)
-        .with_collector_endpoint(base_config.zipkin.clone())
-        .with_trace_config(trace_config)
-        .install_batch(opentelemetry::runtime::Tokio)
-        .unwrap();
+    // a layer for logging based on the requested log level.
+    let log_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(atty::is(Stream::Stderr))
+        .with_filter(loglevel);
 
-    let registry = Registry::default()
-        .with(
-            tracing_subscriber::fmt::layer()
-                .with_ansi(atty::is(Stream::Stderr))
-                .with_filter(loglevel),
-        )
-        .with(tracing_opentelemetry::layer().with_tracer(tracer));
-
-    // TODO maybe move this out here, or put it into some kind of guard / let the app handle this.
-    tracing::subscriber::set_global_default(registry).expect("set global tracer");
-
-    tracing::info!("Initializing zipkin tracing to {:?}", base_config.zipkin);
+    Registry::default()
+        .with(dynamic_layer)
+        .with(log_layer)
+        .init();
 
     // extract and return app config
     let config = extract_with_default(config)?;
@@ -94,4 +84,15 @@ pub fn init<C: Default + Serialize + DeserializeOwned>(service_name: &str, confi
     tracing::info!("Starting application {:?} now", service_name);
 
     Ok(config)
+}
+
+pub fn replace_tracing_layer(layer: Option<Box<dyn Layer<Registry> + Send + Sync>>) -> color_eyre::Result<()> {
+    let handler = TRACING_LAYER.read();
+
+    if let Some(handler) = handler.as_ref() {
+        handler.reload(layer)?;
+        return Ok(());
+    }
+
+    Err(color_eyre::eyre::eyre!("tracing handler not yet initialized"))
 }
---
+++ startup-monitoring/src/lib.rs
@@ -0,0 +1,39 @@
+use eyre::Result;
+use opentelemetry::sdk::trace;
+use serde::{Deserialize, Serialize};
+
+mod idgenerator;
+
+#[derive(Default, Serialize, Deserialize)]
+pub struct MonitoringConfig {
+    zipkin: Option<String>,
+    // statsd: HostPort,
+}
+
+impl MonitoringConfig {
+    pub fn setup(&self, service_name: &str) -> Result<()> {
+        if let Some(zipkin) = self.zipkin.as_ref() {
+            tracing::info!("Setup zipkin tracing to {}", zipkin);
+
+            opentelemetry::global::set_text_map_propagator(opentelemetry_zipkin::Propagator::new());
+
+            let trace_config = trace::Config::default()
+                .with_id_generator(idgenerator::IdGenerator64);
+
+            let tracer = opentelemetry_zipkin::new_pipeline()
+                .with_service_name(service_name)
+                .with_collector_endpoint(zipkin)
+                .with_trace_config(trace_config)
+                .install_batch(opentelemetry::runtime::Tokio)
+                .unwrap();
+
+            // inject layer into registry
+            let layer = tracing_opentelemetry::layer().with_tracer(tracer);
+            startup_base::replace_tracing_layer(Some(Box::new(layer)))?;
+        }
+
+        Ok(())
+    }
+}
+
+
